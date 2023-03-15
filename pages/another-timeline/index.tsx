8458211f1e028f2be15583d27296d303c6cefe10
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2023-03-16 01:37:17 +0900

    move index file

--- pages/another-timeline/index.tsx
+++ pages/another-timeline/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import { useState } from 'react'
 
+import TypeWriter from '@/features/typewriterEffect/components/typewriter';
+
 const getTwHz = async () => {
   const response = await fetch("https://xtmfnoys41.execute-api.us-east-1.amazonaws.com/dev/tw_hz");
   const res = await response.json();
@@ -8,7 +10,7 @@ const getTwHz = async () => {
 }
 
 export default function Home() {
-  const [twHz, setTwHz] = useState();
+  let [twHz, setTwHz] = useState("");
 
   const handleClick = async () => {
     const res = await getTwHz();
@@ -17,19 +19,27 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>CHARNEL-HORSE</title>
+        <title>ANOTHER-TIMELINE</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <main className="bg-black min-h-screen py-0 px-2 flex flex-col justify-center items-center">
-        <div className='text-gray-50 text-3xl cp'>
-          これは過去のTIMELINEの履歴により、次の履歴への幾千の分岐を作り出す。<br />
-          また、隣接次元への漏れがあろうとも、現在のTIMELINEを維持する試みである。<br />
-          千年の断章を閉じることなく、言葉は言葉を生み、やがて、TIMELINEを生み出す輪廻となるだろう<br />
+      <main className="bg-black min-h-screen flex justify-center">
+        <div className='max-w-6xl'>
+
+          <div className='text-gray-50 text-2xl pt-10 pl-4 font-thin'>
+            TWEET HZ GENERATOR
+          </div>
+          <div className='text-gray-50 text-6xl pt-4 font-thin'>
+            ANOTHER - TIMELINE
+          </div>
+          <div className='p-4 text-gray-50 text-2xl'>
+            <TypeWriter data={["TIMELINEの終わり、それが訪れるその日に向け、多学なアヨカヨによる半生命的なトリックを活用した装置。\nこれは長の過去のTIMELINEの履歴により、次の履歴への幾千の分岐を作り出す試みであり、\nまた、隣接次元への漏れがあろうとも、現在のTIMELINEを維持する試みでもある。\n千年の断章を閉じることなく、言葉は言葉を生み、やがて、TIMELINEを生み出す輪廻となるだろう。"]}/>
+
+          </div>
+          <div className="text-2xl">
+            <TypeWriter data={[twHz]}/>
+          </div>
+          <button className='text-gray-50' onClick={handleClick}>Tweet(恥)を生成</button>
         </div>
-        <div className="text-8xl text-gray-600">
-          <span>{twHz ? twHz : ""}</span>
-        </div>
-        <button className='text-gray-50' onClick={handleClick}>Tweet(恥)を生成</button>
       </main>
     </>
   )
