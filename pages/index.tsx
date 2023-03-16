207861d5f1989005e242c007abe9e27ff1299fe1
iWonder118 <52240372+iWonder118@users.noreply.github.com>
2023-03-17 01:07:56 +0900

    update top page style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
 
-const getTwHz = async () => {
-  const response = await fetch("https://xtmfnoys41.execute-api.us-east-1.amazonaws.com/dev/tw_hz");
-  const res = await response.json();
-  return res;
-}
-
 export default function Home() {
   return (
     <>
@@ -14,10 +8,29 @@ export default function Home() {
         <title>CHARNEL-HORSE</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <main className="text-3xl">
-      <Link href="/another-timeline">
-        ANOTHER-TIMELINE
-      </Link>
+      <main className="bg-[url('/charnel_horse/back_image.png')] bg-cover  min-h-screen">
+        <header className="bg-white shadow">
+          <nav className="container mx-auto flex justify-between items-center py-4">
+            <a href="#" className="text-2xl font-bold">CHARNEL - HORSE</a>
+          </nav>
+        </header>
+        {/* <body className="py-10 ">
+          <div className="container mx-auto px-4">
+            <div className="grid grid-cols-1 gap-8">
+              <div className="bg-gray-200 bg-opacity-40" >
+                <h2 className="text-3xl font-bold text-gray-800 mb-4">みんんさ　ご！</h2>
+                <p className="text-gray-700 leading-relaxed mb-6">このサイトは平沢進をモチーフに面白いことができないかと試行錯誤した覚書集です。</p>
+              </div>
+              <div className="bg-gray-200 bg-opacity-40">
+                <h2 className="text-3xl font-bold text-gray-800 mb-4">ANOTHER-TIMELINE</h2>
+                <p className="text-gray-700 leading-relaxed mb-6">過去のTweetを元に発言しそうな文字列生成をします。</p>
+                <Link href="/another-timeline" className="mt-5 px-4 py-2 bg-gray-500 rounded-md hover:bg-gray-600 focus:outline-none focus:shadow-outline-gray text-gray-50 bg-opacity-50 bg-opacity-50 border">
+                  ANOTHER-TIMELINE
+                </Link>
+              </div>
+            </div>
+          </div>
+        </body> */}
       </main>
     </>
   )
